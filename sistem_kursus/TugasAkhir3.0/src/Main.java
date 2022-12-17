ac01f476cefd2bbf13d7be7301999b3757d018b8
RyanGA01 <93762235+RyanGA01@users.noreply.github.com>
2022-12-17 08:10:52 +0700

    Delete sistem_kursus directory
    delete

--- sistem_kursus/TugasAkhir3.0/src/Main.java
+++
@@ -1,8 +0,0 @@
-import view.HomePage;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new HomePage().home();
-    }
-}
-
--- sistem_kursus/TugasAkhir3.0/src/model/ModelObject.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import model.KursusModel;
-import model.Siswa;
-
-public class ModelObject {
-    public static Siswa siswa = new Siswa();
-    public static KursusModel modelKursus = new KursusModel();
-}
