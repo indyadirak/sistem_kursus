24dcd84bd4d3cbff48db69e1b696953ad5cb50d2
Ryan <ryanrga01@gmail.com>
2022-12-23 11:22:54 +0700

    Update LoginPage

--- TugasAkhir3.0/src/view/homepageview/LoginPage.java
+++ TugasAkhir3.0/src/view/homepageview/LoginPage.java
@@ -12,22 +12,45 @@ public class LoginPage {
     public ArrayList<String> arrNpm = new ArrayList<>();
     public int cekdata;
 
-    public void login() {
-
-        try {
-            do {
-                System.out.print("Masukkan NPM anda      : ");
-                String arrNpm = inp.next();
-                System.out.print("Masukkan password      : ");
-                String password = inp.next();
-                cekdata = sC.cekdataAkun(arrNpm, password);
-            } while (cekdata == 0);
-            // Login Problem, Credential is not working
-            System.out.println("Debug test : " + cekdata);
-            System.out.println("\nSelamat datang " + sC.getData(cekdata).getNama() + "\n");
-            new DashboardPage().dashboard();
-        } catch (Exception e) {
-            System.out.println("Nama atau password salah");
+    public void login(){
+        while(true){
+            char pilihan;
+            System.out.println("=========LOGIN=========");
+            System.out.println("Sudah punya akun?");
+            System.out.print("Y/y || N/n: ");
+            pilihan = inp.next().charAt(0);
+            inp.nextLine();
+            if (pilihan == 'y' || pilihan == 'Y'){
+                try {
+                    System.out.print("Masukkan NPM anda      : ");
+                    String arrNpm = inp.next();
+                    System.out.print("Masukkan password      : ");
+                    String password = inp.next();
+                    cekdata = sC.cekdataAkun(arrNpm, password);
+                    if(cekdata != 0){
+                        System.out.println("Debug test : " + cekdata);
+                        System.out.println("\nSelamat datang " + sC.getData(cekdata).getNama() + "\n");
+                        new DashboardPage().dashboard();
+                    }
+                    else{
+                        loginGagal();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Nama atau password salah");
+                }
+            }
+            else if (pilihan == 'n' || pilihan == 'N'){
+                new RegistrationPage().reg();
+            }
+            else{
+                new LoginPage();
+            }
         }
+    }
+
+    private void loginGagal() {
+        System.out.println("-------------------------------");
+        System.out.println("----------LOGIN GAGAL----------");
+        System.out.println("-------------------------------");
     }
 }
